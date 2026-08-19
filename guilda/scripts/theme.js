3fd96c279967ebe1dc348909d8cb76ff88bf17b7
Alan Lima <alanlimanvc@gmail.com>
2026-08-19 19:26:05 -0300

    theme: implement light and dark mode

---
+++ guilda/scripts/theme.js
@@ -0,0 +1,66 @@
+const ThemeClass = {
+    "light": "--light",
+    "dark": "--dark",
+}
+
+const ThemeText = {
+    "light": "[Dark]",
+    "dark": "[Light]",
+}
+
+function main() {
+    const button = document.querySelector("#theme")
+    if (button !== null) {
+        button.addEventListener("click", () => Listener(button))
+        button.textContent = ThemeText[CurrentTheme()]
+    }
+}
+
+function Listener(element) {
+    const html = document.documentElement
+    const theme = SwapTheme()
+
+    html.classList.remove(ThemeClass["light"], ThemeClass["dark"])
+    html.classList.add(ThemeClass[theme])
+    sessionStorage.setItem("theme", theme)
+
+    if (element !== null) {
+        element.classList.remove(ThemeClass["light"], ThemeClass["dark"])
+        element.classList.add(ThemeClass[theme])
+        element.textContent = ThemeText[theme]
+    }
+}
+
+function SwapTheme() {
+    switch (CurrentTheme()) {
+    case "dark":
+        return "light"
+    case "light":
+        return "dark"
+    }
+}
+
+function CurrentTheme() {
+    const html = document.documentElement
+
+    if (html.classList.contains(ThemeClass["light"])) {
+        return "light"
+    }
+    if (html.classList.contains(ThemeClass["dark"])) {
+        return "dark"
+    }
+
+    const theme = sessionStorage.getItem("theme")
+    if (theme === "light" || theme === "dark") {
+        return theme
+    }
+
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        return "dark"
+    }
+
+    return "light"
+}
+
+document.documentElement.classList.add("--" + CurrentTheme())
+document.addEventListener("DOMContentLoaded", main)
