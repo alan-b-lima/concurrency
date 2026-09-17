033d423a4a5ca10b6bedca9e9ea07cd8ccdb9f70
Alan Lima <alanlimanvc@gmail.com>
2026-09-17 10:32:36 -0300

    snake: improve IOControl template

--- examples/snake/src/main/java/dev/snake/IOControl.java
+++ examples/snake/src/main/java/dev/snake/IOControl.java
@@ -1,18 +1,23 @@
 package dev.snake;
 
 import java.io.IOException;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.ReentrantLock;
+import java.nio.charset.Charset;
 
+import org.jline.terminal.Attributes;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
-import org.jline.utils.NonBlockingReader;
 
 public class IOControl {
     private final Terminal terminal;
+    private final Attributes attributes;
 
     public IOControl() throws IOException, InterruptedException {
-        this.terminal = TerminalBuilder.builder().system(true).build();
+        this.terminal = TerminalBuilder.builder()
+            .system(true)
+            .encoding(Charset.forName("UTF-8"))
+            .build();
+
+        this.attributes = this.terminal.enterRawMode();
         /* implement me */
     }
 
@@ -26,18 +31,16 @@ public class IOControl {
 
     public boolean hasInput() {
         /* implement me */
+        return false;
     }
 
     public char input() {
         /* implement me */
+        return 0;
     }
 
     public void close() {
         /* implement me */
-    }
-
-    private void routine() {
-        this.terminal.enterRawMode();
-        /* implement me */
+        this.terminal.setAttributes(this.attributes);
     }
 }
